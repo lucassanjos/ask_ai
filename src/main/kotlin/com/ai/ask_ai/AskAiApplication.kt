5413471ff553b27e542c1fe6326923c8f87b12e3
Lucas Anjos <banjoslucas@outlook.com>
2026-02-10 22:34:44 -0300

    Primeiro Commit

---
+++ src/main/kotlin/com/ai/ask_ai/AskAiApplication.kt
@@ -0,0 +1,11 @@
+package com.ai.ask_ai
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AskAiApplication
+
+fun main(args: Array<String>) {
+	runApplication<AskAiApplication>(*args)
+}
